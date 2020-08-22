f98f6e26a2fbdd75437c238b6638ecb308f9157d
Idc <59904122+IDontCare-05@users.noreply.github.com>
2020-08-22 10:22:49 +0700

    Clock Style : Add lockscreen Loading animation clock
    https://github.com/ZenX-OS/android_frameworks_base/commit/8772f704e0d2d59df32909b5aa447c4f57900c68

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -161,6 +161,10 @@ public final class ClockManager {
         addBuiltinClock(() -> new MNMLBoxClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new MNMLMinimalClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SwingClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new BubbleAnimationClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new RainbowCircleClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new FireCircleClockController(res, layoutInflater, colorExtractor, context));
+        addBuiltinClock(() -> new LoadingClockController(res, layoutInflater, colorExtractor, context));
 
         // Store the size of the display for generation of clock preview.
         DisplayMetrics dm = res.getDisplayMetrics();
